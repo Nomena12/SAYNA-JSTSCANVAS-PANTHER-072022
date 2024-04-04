6ca34772049e5c256fe3775a6d7dcede291c0c62
Ramarolahy <nomenaramarolahy@yahoo.com>
2024-04-04 04:45:47 +0200

    amelioration des fichiers

--- index.js
+++ index.js
@@ -6,3 +6,43 @@ submitBtn.addEventListener('click',()=>{
     popUp.style.opacity = "1";
 });
 
+const voirPlink = document.querySelector('.in1 a');
+voirPlink.addEventListener("click",()=>{
+voirPlink.setAttribute("href","#prince");
+});
+
+
+// Get the element to animate
+const element = document.querySelector('.my-element');
+const element1 = document.querySelector('.my');
+
+
+// Define the options for the Intersection Observer
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.6
+};
+
+// Create a new Intersection Observer
+const observer = new IntersectionObserver(function(entries, observer) {
+  entries.forEach(entry => {
+    // If element is in viewport, add the 'show' class to trigger the animation
+    if (entry.isIntersecting) {
+      element.classList.add('show');
+      element1.classList.add('show');
+
+    }
+    else {
+      element.classList.remove('show');
+      element1.classList.remove('show');
+
+    }
+  });
+}, options);
+
+// Start observing the element
+observer.observe(element);
+observer.observe(element1);
+
+
